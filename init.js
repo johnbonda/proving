0996e3c9a8d009ba9f91e526c1a7c8a16757dc60
johnbonda <john@belfricsbt.com>
2018-11-02 11:48:53 +0530

    Just to db

--- init.js
+++ init.js
@@ -3,6 +3,10 @@ module.exports = async function () {
 
   app.registerContract(1000, 'domain.register')
   app.registerContract(1001, 'domain.set_ip')
+  app.registerContract(1003, 'payroll.issuePaySlip')
+  app.registerContract(1004, 'payroll.verify')
+  app.registerFee(1003, '0', 'BEL')
+  app.registerFee(1004, '0', 'BEL')
 
 
   app.events.on('newBlock', (block) => {

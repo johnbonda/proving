b5ed7e2432b3adbdd6b5e5b4ab1520112d9fbf46
johnbonda <john@belfricsbt.com>
2018-11-10 12:42:41 +0530

    Chandini changes

--- interface/registrations.js
+++ interface/registrations.js
@@ -3,6 +3,7 @@ var httpCall = require('../utils/httpCall.js');
 var constants = require('../utils/constants.js');
 var addressHelper = require('../utils/address.js');
 var z_schema = require('../utils/zschema-express.js');
+var BKVSCall = require('../utils/BKVSCall.js');
 
 
 // Return Payslip with empname
@@ -75,4 +76,53 @@ app.route.post('/accounts/details', async function(req,cb) {
      return res;
 });
 
+//BKVS login
+app.route.post('/login', async function (req, cb) {
+    var ac_params = {
+        email: req.query.email,
+        password: req.query.password
+    };    var response = await BKVSCall.call('POST', `/api/v1/login`, ac_params);// Call: http://54.254.174.74:8080
+    if(response.isSuccess===true)
+    {
+        return "success";
+    }
+    else
+    {
+        return "failed";
+    }
+ });//BKVS Signup
+ app.route.post('/signup', async function (req, cb) {
+    var params={
+        countryId:req.query.countryId,
+        countryCode:req.query.countryCode,
+        email:req.query.emailid,
+        name:req.query.name,
+        password:req.query.password,
+        type:req.query.type
+    }
+    var response = await BKVSCall.call('POST', `/api/v1/signup`, params);// Call: http://54.254.174.74:8080
+    if(response.isSuccess===true)
+    {
+        return "success";
+    }
+    else
+    {
+        return "failed";
+    }
+ });//BKVS Signup
+ app.route.post('/secretLogin', async function (req, cb) {
+    var params={
+        secret:req.query.secret,
+        countryCode:req.query.countryCode
+    }
+    var response = await BKVSCall.call('POST', `/api/v1/hyperledger/login`, params);// Call: http://54.254.174.74:8080
+    if(response.isSuccess===true)
+    {
+        return "success";
+    }
+    else
+    {
+        return "failed";
+    }
+ });
 // KYC Verification``` 
--- utils/constants.js
+++ utils/constants.js
@@ -3,6 +3,7 @@ module.exports = {
   defaultCurrency: 'BEL', // default currency symbole for Belrium
   totalSupply: 2100000000000000000,
   URL: "http://localhost:9305",
+  URI: "http://54.254.174.74:8080",
   fees: {
     send: 0.001,
     inTransfer: 0.001,
